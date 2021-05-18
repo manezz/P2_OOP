281bf1d2783746cb67a1f9d7ded0980cfdc97bce
Magnus Vangsgaard Norling <68356591+manezz@users.noreply.github.com>
2021-05-18 22:19:05 +0200

    Add files via upload

---
+++ Opg1/Opgave 1 (Inheritance)/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Opgave_1
+{
+	class BilVask
+	{
+		// Til nedarvning
+		protected int budgetVask;
+		protected int sølvVask;
+		protected int guldVask;
+	}
+
+	class Program : BilVask
+	{
+		static void Main(string[] args)
+		{
+			var vask = new Program();
+
+			// Nedarvede klasser får en værdi	
+			vask.budgetVask = 20;
+			vask.sølvVask = 50;
+			vask.guldVask = 100;
+
+			Console.WriteLine("Buget Vask " + vask.budgetVask + " Kr.");
+			Console.WriteLine("Sølv Vask " + vask.sølvVask + " Kr.");
+			Console.WriteLine("Guld Vask " + vask.guldVask + " Kr.");
+		}
+	}
+}
---
+++ Opg1/Opgave 2 (Abstraction)/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Opgave_2
+{
+	abstract class Food
+	{
+		public abstract void foodIsReady();
+
+		public void foodIsNotReady()
+		{
+			Console.WriteLine("Food is not ready");
+		}
+	}
+	class Restaurant : Food
+	{
+		public override void foodIsReady()
+		{
+			Console.WriteLine("Food is ready");
+		}
+	}
+
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Restaurant myRestaurant = new Restaurant();
+			myRestaurant.foodIsReady();
+			myRestaurant.foodIsNotReady();
+		}
+	}
+}
